071683131f7bcee5ab2b97b56d168e7d6fda847d
Louis Pallett <louis.pallett@outlook.com>
2023-10-29 16:30:56 +0000

    Implemented removeNode() so now nodes can be removed

--- src/js/functionality.js
+++ src/js/functionality.js
@@ -39,7 +39,59 @@ const removeNode = (root, value) => {
       If value to delete = 70, then we need to find the child (or grandchild or great grand child and so on) which is
       closest to 71 or 71 itself. 
       71 (or the closest number) replaces 75.*/
-      return 1;
+
+  // Base case
+  if(root === null) {
+    return root;
+  }
+
+  if(root.data > value) {
+    root.left = removeNode(root.left, value);
+    return root;
+  } else if (root.data < value) {
+    root.right = removeNode(root.right, value);
+    return root
+  }
+
+  // At this point, root == value
+  // If one children is empty
+  if(root.left === null) {
+    let temp = root.right;
+    delete root.data;
+    return temp;
+  } else if(root.right === null) {
+    let temp = root.left;
+    delete root.data;
+    return temp;
+  } 
+  // Both children exist
+  else {
+    let succParent = root;
+    let succ = root.right;
+    while(succ.left !== null) {
+      succParent = succ;
+      succ = succ.left;
+    }
+
+    // Delete successor.  Since successor
+    // is always left child of its parent
+    // we can safely make successor's right
+    // right child as left of its parent.
+    // If there is no succ, then assign
+    // succ.right to succParent.right
+    if(succParent !== root) {
+      succParent.left = succ.right;
+    } else {
+      succParent.right = succ.right;
+    }
+    
+    // Copy Successor Data to root
+    root.data = succ.data;
+
+    // Delete Successor and return root
+    delete succ.data;
+    return root;
+  }
 };
 
 const findNode = (root, value) => {
--- src/js/main.js
+++ src/js/main.js
@@ -47,12 +47,27 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     }
 };
 
-const testArray = [7, 2, 3, 5, 1, 6, 4, 8, 10, 9, 15, 11, 14, 12, 13]
+// const testArray = [7, 2, 3, 5, 1, 6, 4, 8, 10, 9, 15, 11, 14, 12, 13]
+// const conciseArray = removeDuplicates(testArray);
+// const sortedArray = sort(conciseArray);
+// const tree = new Tree(sortedArray);
+// console.log(tree);
+// prettyPrint(tree.root);
+
+// insertNode(tree.root, -1);
+// prettyPrint(tree.root);
+
+const testArray = [55, 12, 88, 12, 45, 4, 5, 7, 88, 97, 94, 66, 523, 1, 123, 71, 49];
 const conciseArray = removeDuplicates(testArray);
 const sortedArray = sort(conciseArray);
+console.log(sortedArray);
 const tree = new Tree(sortedArray);
-console.log(tree);
 prettyPrint(tree.root);
 
-const newRoot = insertNode(tree.root, 16);
+
+insertNode(tree.root, -1);
+insertNode(tree.root, 64);
+prettyPrint(tree.root);
+
+removeNode(tree.root, 4);
 prettyPrint(tree.root);
